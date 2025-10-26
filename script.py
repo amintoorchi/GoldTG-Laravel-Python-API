1774fd2ebd650e9bcc01e94a01a86cf64095e6db
Amin Toorchi <77411398+amintoorchi@users.noreply.github.com>
2025-10-27 01:44:31 +0330

    initial commit

--- script.py
+++ script.py
@@ -1,12 +1,10 @@
 import requests
 
-# 1. گرفتن قیمت از سایت
 url = "https://tabangohar.com/GheymatKhan/prices_in_table.html"
 response = requests.get(url)
 data = response.json()
 price = data["c"]
 
-# 2. فرستادن به API لاراول
 api_url = "https://example.com/api/store-price"
 payload = {
     "price": price
